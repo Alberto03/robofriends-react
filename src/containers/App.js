596b2ede8c697d10f6b4dc065958fcd54ea6b3de
alberto03 <alberto.abis1@gmail.com>
2021-06-02 13:26:39 +0200

    correct mispell

--- src/containers/App.js
+++ src/containers/App.js
@@ -28,7 +28,7 @@ class App extends Component {
     render() {
         const {robots, searchfield} = this.state;
         const filteredRobots = robots.filter( robot => {
-            return robot.name.toLocaleLowerCase().includes(searchfield.toLowerCase());
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         });
 
         if(!robots.length){
